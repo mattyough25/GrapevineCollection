f7d0cb5ea85f0dc39e9a76e6032fd44652549011
mattyough25 <mgy0003@mix.wvu.edu>
2023-01-09 10:34:18 -0500

    Update runGrapevine_C.m

--- runGrapevine_C.m
+++ runGrapevine_C.m
@@ -4,33 +4,33 @@ clc
 
 global DF
 sPath = cd;
-%% Initialize Boolean Variables
+%% Inistialize Boolean Variables
 bRun = true;
 bWrite2File = true;
 
 %% Set File Name
 sDataPath = cd;
-sFile = [sprintf('GRAPEVINE_Array_BB_VR_2_%s', datestr(now,'mm-dd-yyyy-HH-MM-SS'))];
+sFile = [sprintf('GRAPEVINE_Array_Test_1_%s', datestr(now,'mm-dd-yyyy-HH-MM-SS'))];
 sFilePath = [sDataPath,filesep,sFile];
-sFileMat = sprintf('GRAPEVINE_Array_BB_VR_2_%s.mat', datestr(now,'mm-dd-yyyy-HH-MM-SS'));
+sFileMat = sprintf('GRAPEVINE_Array_Test_1_%s.mat', datestr(now,'mm-dd-yyyy-HH-MM-SS'));
 
 %% Define ip_address
-ip = ['10.3.6.85'];
+ip = ['[]'];
 
 %% Subscribe to DF Messages
 % add messages
-MessageTypes =  {'SERVER_TIMESTAMP_USER'};
-% Messages to subscribe to DF
-ConnectArgs = {0, [], 'message_defs_wvu.mat'};
-mm_ip = ip;
-if strcmp(mm_ip, '[]')
-    mm_ip = [];
-end
-if exist('mm_ip','var') && ~isempty(mm_ip)
-    ConnectArgs{end+1} = ['-server_name ' mm_ip, ':7111'];
-end
-ConnectToMMM(ConnectArgs{:});
-Subscribe( MessageTypes{:});
+% MessageTypes =  {'SERVER_TIMESTAMP_USER'};
+% % Messages to subscribe to DF
+% ConnectArgs = {0, [], 'message_defs_wvu.mat'};
+% mm_ip = ip;
+% if strcmp(mm_ip, '[]')
+%     mm_ip = [];
+% end
+% if exist('mm_ip','var') && ~isempty(mm_ip)
+%     ConnectArgs{end+1} = ['-server_name ' mm_ip, ':7111'];
+% end
+% ConnectToMMM(ConnectArgs{:});
+% Subscribe( MessageTypes{:});
 
 %% Run Record Executable
 record = actxserver('WScript.Shell');
@@ -62,42 +62,49 @@ pause(0.1)
 %% Connect to Dragonfly and Get TimeServer Time
 if bRun
     
-    %% read timeserver timestamp
-    % Request Time Server Time
-    nMT  = EnsureNumericMessageType('REQUEST_TIMESTAMP_USER');
-    % Get Sending Time
-    msg         = DF.MDF.REQUEST_TIMESTAMP_USER;
-    UnsafeSendMessage( nMT, msg);
+%     %% read timeserver timestamp
+%     % Request Time Server Time
+%     nMT  = EnsureNumericMessageType('REQUEST_TIMESTAMP_USER');
+%     % Get Sending Time
+%     msg         = DF.MDF.REQUEST_TIMESTAMP_USER;
+%     UnsafeSendMessage( nMT, msg);
     
-    for iMessage = 1:2
-        M           = ReadMessage('blocking');
-        if ~isempty(M.data)
-            record.SendKeys('{ENTER}');
-            break;
-        end
-    end
-    
-    nDataGrapevine.tTime(1) = M.data.t;
+%     for iMessage = 1:2
+%         M           = ReadMessage('blocking');
+%         if ~isempty(M.data)
+%             record.SendKeys('{ENTER}');
+%             break;
+%         end
+%     end
+    record.SendKeys('{ENTER}'); 
     
     bWaitbar = waitbar(0, 'Grapevine EMG', 'Name', 'Collecting Data','CreateCancelBtn','delete(gcbf)');
     
     ix = 1;
-    tic
     while bRun
         drawnow;
-        nDataGrapevine.tCountTime(ix) = toc;
+        
+        %% Get Current Universal Time
+        if ix == 1
+            sTime = split(string(datetime('now','TimeZone','UTC','Format','HH:mm:ss.SSS')),":");
+            tTime = (str2double(sTime(1))*3600) + (str2double(sTime(2))*60) + str2double(sTime(3));
+	    tic
+            nDataGrapevine.tTime(1) = tTime;
+        else
+            nDataGrapevine.tTime(ix) = nDataGrapevine.tTime(1) + toc;
+        end
         ix = ix + 1;
         
         if ~ishandle(bWaitbar)
             record.SendKeys('s{ENTER}x{ENTER}');
             
             % Request Time Server Time
-            nMT  = EnsureNumericMessageType('REQUEST_TIMESTAMP_USER');
-            % Get Sending Time
-            msg         = DF.MDF.REQUEST_TIMESTAMP_USER;
-            UnsafeSendMessage( nMT, msg);
-            M           = ReadMessage('blocking');
-            nDataGrapevine.tTime(2) = M.data.t;
+%             nMT  = EnsureNumericMessageType('REQUEST_TIMESTAMP_USER');
+%             % Get Sending Time
+%             msg         = DF.MDF.REQUEST_TIMESTAMP_USER;
+%             UnsafeSendMessage( nMT, msg);
+%             M           = ReadMessage('blocking');
+%             nDataGrapevine.tTime(ix) = nDataGrapevine.tTime(1) + toc;
             
             disp('Stopped Recording')
             break;
